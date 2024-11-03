58d475cd523c49360816426f56bea8347f41a8da
Zeta-05 <vjimmy276@gmail.com>
2024-11-03 15:32:54 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "7038202445"
     sudo_users = "7757912959", "7783550524" , "7038202445"
     GROUP_ID = -1002324830715
-    TOKEN = "8133429097:AAFIRfJjiwmMmmnRCkyjBk0UHxUU_t2n3uM"
+    TOKEN = "8133429097:AAFCxOWcVoFA_KrMlRlLnh79UuEplNZ6o1c"
     mongo_url = "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://graph.org/file/34ce254537a31ca2a788b-f8b59bff546af8dc58.jpg", "https://graph.org/file/0cd48cfd409203a00da74-2a919fe99dd1b238ec.jpg"]
     SUPPORT_CHAT = "https://t.me/Anime_Group_India_Chat"
